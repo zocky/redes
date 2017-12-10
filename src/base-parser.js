13d62bfb155b518e70545c185b97e02322a05358
Zoran Obradović <zockyo@gmail.com>
2017-12-10 02:54:42 +0100

    started expected

--- src/base-parser.js
+++ src/base-parser.js
@@ -44,7 +44,7 @@ module.exports = function Redes () {
     }
     var res = R$START(state);
     if (state.pos!==text.length) {
-      throw new Error(`Syntax error at [${$state.$line}:${$state.$col}]: ${text.substr(text,20)}, expected one of ${state.expect}`)
+      throw new Error(`Syntax error at [${$state.$line}:${$state.$col}]: ${text.substr(text,20)}, expected one of ${state.expected}`)
     }
     return res[0];
   }
